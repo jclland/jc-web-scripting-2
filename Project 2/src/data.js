e7f401e6131e20e4cec11adb4459dd94461b50a1
jclland <jcarnahan@lakelandcollege.edu>
2026-06-26 17:31:37 -0500

    Uploaded all of project 2 files

---
+++ Project 2/src/data.js
@@ -0,0 +1,67 @@
+export const welcomeMessage = "Welcome to my Web Scripting II starter app";
+
+//semester goals
+export const goals = [
+    "Get comfortable using Vite and npm",
+    "Have a better understanding of JavaScript close to/on par with my Python knowledge",
+    "Focus on making cleaner looking projects",
+    "Pass the class with an A"
+];
+
+//already known skill
+export const knownSkills = [
+    "Python",
+    "Basic HTML, CSS",
+    "A little bit of JavaScript",
+    "Content Management Systems",
+    "Project Management",
+    "Game Development in the Godot engine"
+];
+
+//want to learn
+export const newSkills = [
+    "Vite/npm",
+    "Better JavaScript knowledge",
+    "More Familiarity With APIs",
+    "Database Implementation"
+];
+
+//web projects for the semester
+export const projects = [
+    {
+        title: "Project 1: State-Driven Dashboard App",
+        description: "Build a simple, state-driven dashboard app using HTML, CSS, and JavaScript.",
+        status: "Done"
+    },
+    {
+        title: "Project 2: Vite Starter",
+        description: "Create my first modern JavaScript project using Node.js, npm, and Vite.",
+        status: "Done"
+    },
+    {
+        title: "Project 3",
+        description: "N/A",
+        status: "Not started"
+    },
+    {
+        title: "Project 4",
+        description: "N/A",
+        status: "Not started"
+    },
+    {
+        title: "Project 5",
+        description: "N/A",
+        status: "Not started"
+    },
+        {
+        title: "Project 6",
+        description: "N/A",
+        status: "Not started"
+    },
+        {
+        title: "Project 7",
+        description: "N/A",
+        status: "Not started"
+    }
+    
+];
